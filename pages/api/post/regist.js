2730d2d85c82d69bf04bf79cd605ea0ccbd974a8
kirumel <kirumel7621@gmail.com>
2024-05-29 00:41:30 +0900

    0.382

---
+++ pages/api/post/regist.js
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import sendVerificationEmail from "../../../util/sendemail";
+import crypto from "crypto";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password, name, nickname } = req.body;
+
+    if (!email || !password || !name || !nickname) {
+      return res.status(400).json({ message: "모든 필드를 입력해주세요" });
+    }
+
+    const generateToken = () => {
+      const token = crypto.randomBytes(32).toString("hex");
+      const expires = new Date();
+      expires.setHours(expires.getHours() + 24);
+      return { token, expires };
+    };
+
+    const { token, expires } = generateToken();
+
+    try {
+      await sendVerificationEmail(email, expires, token);
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await prisma.unverifiedUser.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+          nickname,
+          token,
+          expires,
+        },
+      });
+
+      res.status(201).json({ message: "이메일에 인증 링크가 발송되었습니다." });
+    } catch (error) {
+      console.error("회원 가입 중 오류 발생:", error);
+      res.status(500).json({ message: "회원 가입 중 오류가 발생했습니다." });
+    }
+  } else {
+    res.status(405).json({ message: "허용되지 않는 메서드입니다." });
+  }
+}
---
+++ pages/api/verify-email.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { email, token } = req.query;
+
+    if (!email || !token) {
+      return res.status(400).json({ message: "유효하지 않은 요청입니다." });
+    }
+    try {
+      const unverifiedUser = await prisma.unverifiedUser.findUnique({
+        where: { email },
+      });
+
+      if (
+        !unverifiedUser ||
+        unverifiedUser.token !== token ||
+        new Date() > unverifiedUser.expires
+      ) {
+        return res
+          .status(400)
+          .json({
+            message: "이미 인증된 이메일이거나 유효기간이 만료된 링크입니다",
+          });
+      }
+
+      // 이메일 중복 체크
+      const existingUser = await prisma.sJHSUser.findUnique({
+        where: { email },
+      });
+
+      if (existingUser) {
+        return res.status(400).json({ message: "이미 등록된 이메일입니다." });
+      }
+
+      const { password, name, nickname } = unverifiedUser;
+
+      const newUser = await prisma.sJHSUser.create({
+        data: { email, password, name, nickname },
+      });
+
+      await prisma.unverifiedUser.delete({
+        where: { email },
+      });
+
+      res.status(200).json({
+        message: "이메일 인증 성공. 회원가입이 완료되었습니다.",
+        user: newUser,
+      });
+    } catch (error) {
+      console.error("이메일 인증 중 오류 발생:", error);
+      res.status(500).json({ message: "이메일 인증 중 오류가 발생했습니다." });
+    }
+  } else {
+    res.status(405).json({ message: "허용되지 않는 메서드입니다." });
+  }
+}
