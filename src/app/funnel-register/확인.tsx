4c4237df024c8d6c390a7e5055581c95f48cc07e
kirumel <kirumel7621@gmail.com>
2024-06-07 23:15:59 +0900

    0.388

--- src/app/funnel-register/확인.tsx
+++ src/app/funnel-register/확인.tsx
@@ -6,9 +6,9 @@ export default function StartRegister(props: any) {
   const [buttoncomment, setButtoncomment] = useState<string>("");
   useEffect(() => {
     if (showPassword) {
-      setButtoncomment("카드보기");
+      setButtoncomment("비밀번호 보기");
     } else {
-      setButtoncomment("가리기");
+      setButtoncomment("비밀번호 가리기");
     }
   });
 
@@ -21,22 +21,45 @@ export default function StartRegister(props: any) {
             확인해볼까요?
           </h2>
           <p className="subtitle">아래의 정보를 확인해주세요</p>
-          <div>
-            <p>{name}</p>
-            <p>{email}</p>
-            <p>
-              {showPassword ? <span>{password}</span> : null}
-              <span>
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
+          <div className="card-display">
+            <div className="card">
+              <div style={{ display: "flex", justifyContent: "right" }}>
+                <h1
+                  style={{
+                    marginTop: "3rem",
+                    fontSize: "2.3rem",
+                  }}
                 >
-                  {buttoncomment}
-                </button>
-              </span>
-            </p>
-            <p>{nickname}</p>
-            <p>{id}</p>
+                  {name}
+                </h1>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p className="subtitlewhite margin0">이메일</p>
+                <p className="margintop0">{email}</p>
+                <p className="subtitlewhite margin0">비밀번호</p>
+                <p className="margintop0">
+                  {showPassword ? <span>{password}</span> : null}
+                  <span>
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {buttoncomment}
+                    </button>
+                  </span>
+                </p>
+                <p className="subtitlewhite margin0">닉네임</p>
+                <p className="margintop0">{nickname}</p>
+                <p className="subtitlewhite margin0">id</p>
+                <p className="margintop0">{id}</p>
+              </div>
+            </div>
           </div>
           <div className="ok-button-div">
             <form action="/api/post/regist" method="POST">
